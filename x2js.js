f156e6c0651507a3f8714452f5b80d3b5b5c7ebe
Brian Rayburn <bgrayburn@gmail.com>
2017-03-24 16:50:21 -0400

    removing amd option to avoid breaking electron

--- x2js.js
+++ x2js.js
@@ -34,10 +34,7 @@
 	"use strict";
 
 	/* global define */
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
+    if (typeof module === 'object' && module.exports) {
         // Node. Does not work with strict CommonJS, but only CommonJS-like
 		// environments that support module.exports, like Node.
         module.exports = factory(require("xmldom").DOMParser);
